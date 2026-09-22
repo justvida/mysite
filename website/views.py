67c24c0b6ae3de2ff1fd4e79459e3474b54fa56e
justvida <vidahakimi.b@gmail.com>
2026-09-22 21:18:08 +0330

    start creating app

---
+++ website/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse, JsonResponse
+
+def http_test(request):
+    return HttpResponse("<h1>this is a test</h1>")
+
+def json_test(request):
+    return JsonResponse({'name':'ali'})
