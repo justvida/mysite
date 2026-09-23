ddf92fbe217c68785db7f4d7e2c325f2c7944814
justvida <vidahakimi.b@gmail.com>
2026-09-23 15:42:01 +0330

    adding some informations!

--- website/views.py
+++ website/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse, JsonResponse
 
+
 def http_test(request):
     return HttpResponse("<h1>this is a test</h1>")
 
